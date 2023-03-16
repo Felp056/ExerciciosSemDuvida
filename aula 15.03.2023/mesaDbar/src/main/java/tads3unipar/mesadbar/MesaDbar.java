d0f62cd59a7c9879134b19bd4d853275039197d1
Felipe Talini <100320446+Felp056@users.noreply.github.com>
2023-03-15 23:16:52 -0300

    add exercicio da mesa de bar
    mesa de bar onde se pede algo e se recebe a conta pelo pedido

---
+++ aula 15.03.2023/mesaDbar/src/main/java/tads3unipar/mesadbar/MesaDbar.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tads3unipar.mesadbar;
+
+/**
+ *
+ * @author felip
+ */
+public class MesaDbar {
+
+    public static void main(String[] args) {
+       prod p = new prod();
+       p.cadastraProduto();
+    }
+}
